f05b36dfe9503b9cd4f1230792b62694f8d8a91f
amrremam <erorat10@gmail.com>
2024-01-20 22:20:56 +0200

    last init

--- addUser/views.py
+++ addUser/views.py
@@ -10,35 +10,61 @@ class AddUser(View):
         return render(request, self.template_name)
 
     def post(self, request, *args, **kwargs):
-        name = request.POST.get('name')
-        country = request.POST.get('country')
-        phone = request.POST.get('phone')
+        record = Member.objects.all()
+        amana = request.POST.get('amana')
+        partyde = request.POST.get('partyde')
+        amanaQism = request.POST.get('amanaQism')
+        amanaSheyakha = request.POST.get('amanaSheyakha')
+        amanaCairo = request.POST.get('amanaCairo')
+        amanaKetaa = request.POST.get('amanaKetaa')
+        Taam = request.POST.get('Taam')
+        specificLagna = request.POST.get('specificLagna')
+        pastParty = request.POST.get('pastParty')
+        manasebAamma = request.POST.get('manasebAamma')
+        sanaDawra = request.POST.get('sanaDawra')
+        odwyaSabka = request.POST.get('odwyaSabka')
+        userImg = request.POST.get('userImg')
+        presidentPartyName = request.POST.get('presidentPartyName')
+        noaaelAdwiaa = request.POST.get('noaaelAdwiaa')
+        famousName = request.POST.get('famousName')
+        NationalId = request.POST.get('NationalId')
+        age = request.POST.get('age')
+        yearBirth = request.POST.get('yearBirth')
+        monthBirth = request.POST.get('monthBirth')
+        dayBirth = request.POST.get('dayBirth')
+        education = request.POST.get('education')
+        workProfession = request.POST.get('workProfession')
+        nekaba = request.POST.get('nekaba')
+        ketaaAamel = request.POST.get('ketaaAamel')
+        currentlyWorkProfession = request.POST.get('currentlyWorkProfession')
+        martialStatus = request.POST.get('martialStatus')
+        mobileOne = request.POST.get('mobileOne')
+        mobileTwo = request.POST.get('mobileTwo')
+        landline = request.POST.get('landline')
+        email = request.POST.get('email')
+        rakmElmabna = request.POST.get('rakmElmabna')
+        street = request.POST.get('street')
+        district = request.POST.get('district')
+        city = request.POST.get('city')
+        # state = request.POST.get('state')
+        qism = request.POST.get('qism')
+        ketaa = request.POST.get('ketaa')
+        mohafza = request.POST.get('mohafza')
 
-        user = Member(name=name, country=country, phone=phone)
+        print(record)
+
+        user = Member(
+            amana=amana, partyde=partyde, amanaQism=amanaQism,
+            amanaSheyakha=amanaSheyakha, amanaCairo=amanaCairo, amanaKetaa=amanaKetaa,
+            Taam=Taam, specificLagna=specificLagna, pastParty=pastParty, manasebAamma=manasebAamma,
+            sanaDawra=sanaDawra, odwyaSabka=odwyaSabka, userImg=userImg, presidentPartyName=presidentPartyName,
+            noaaelAdwiaa=noaaelAdwiaa, famousName=famousName, NationalId=NationalId, age=age, yearBirth=yearBirth,
+            monthBirth=monthBirth, dayBirth=dayBirth, education=education, workProfession=workProfession,
+            nekaba=nekaba, ketaaAamel=ketaaAamel, currentlyWorkProfession=currentlyWorkProfession,
+            martialStatus=martialStatus, mobileOne=mobileOne, mobileTwo=mobileTwo, landline=landline,
+            email=email, rakmElmabna=rakmElmabna, street=street, district=district, city=city,
+            qism=qism, ketaa=ketaa, mohafza=mohafza,
+        )
         user.save()
 
         return redirect('app:dashboard')
-
-
-def edit_user(request, user_id):
-    user = get_object_or_404(Member, pk=user_id)
-    
-    if request.method == 'POST':
-        user.name = request.POST.get('name')
-        user.lname = request.POST.get('lname')
-        user.age = request.POST.get('age')
-        user.save()
-
-        return render(request, 'test.html', {'user': user})
-
-    return render(request, 'test.html', {'user': user})
-
-
-def delete_user(request, user_id):
-    user = get_object_or_404(Member, pk=user_id)
-    
-    if request.method == 'POST':
-        user.delete()
-        return redirect('dashboard.html')
-
-    return render(request, 'dashboard.html', {'user': user})
